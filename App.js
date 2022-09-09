4b327222d5319cd465aff36c31f2b9aaa64bac2b
Olli Tanhuanpää <ottanh@utu.fi>
2022-09-09 18:34:27 +0300

    tehtävät 10.1-10.2

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+console.log('test')
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! WÖÖÖÖÖÖ</Text>
+      <Text>tuomas on homo hihi</Text>
       <StatusBar style="auto" />
     </View>
   );
